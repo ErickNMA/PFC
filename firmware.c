f5f6fe6341ae5fedfa10063021acb29d6ad03a05
Erick <erick.martins@external.stellantis.com>
2025-10-11 19:24:04 -0300

    LAST UPDATE

--- firmware.c
+++ firmware.c
@@ -60,11 +60,11 @@ const int PM6[2] = {41, 42};	//Motor 6
 #define ts 10				// tempo de amostragem, em milissegundos (amostragem e controlador)
 #define tt 100				// step de tempo para a geração de trajetória
 #define SPD_ANG 10			// velocidade angular (graus por segundo)
-#define SPD_LIN 100		// velocidade linear (mm/s)
-#define SKIP_SAMPLES 10	// frequência de envio para a IHM (a cada 5 amostras reais, 1 é enviada)
+#define SPD_LIN 20			// velocidade linear (mm/s)
+#define SKIP_SAMPLES 10	// frequência de envio para a IHM (a cada 10 amostras reais, 1 é enviada)
 #define BUFF_LEN 1			// tamanho do buffer de envio dos dados
 #define kf 50				// constante do filtro digital de amostragem
-#define L 5					// deslocamento do arranjo diferencial
+#define L 2					// deslocamento do arranjo diferencial
 #define USAT 50				// saturação do sinal de controle
 
 //Constantes padrão para o controlador PID para cada grau de liberdade:
@@ -101,8 +101,12 @@ float ZM[6][3] = {
 		{3.5, 0, 3.5},
 };
 
+//Limites dos graus de liberdade:
+float limits[6][2] = {{-170, 170}, {-90, 30}, {-135, 0}, {-120, 120}, {-170, 170}, {0, 100}};
+
 //Constantes de trajetória:
 float trajconsts[5][4];
+float linsteps[5];
 
 
 
@@ -167,6 +171,8 @@ const int ISS[6] = {ISS1, ISS2, ISS3, ISS4, ISS5, ISS6};
 adc_oneshot_unit_handle_t adc_handle = NULL;
 esp_mqtt_client_handle_t client = NULL;
 esp_timer_handle_t aftertraj_timer;
+float P0[5] = {0, 0, 0, 0, 0};
+float CARTESIAN[5] = {0, 0, 0, 0, 0};
 float TARGETS[6] = {0, 0, 0, 0, 0, 0};
 float REFS[6] = {0, 0, 0, 0, 0, 50};
 float DOFS[6] = {0, 0, 0, 0, 0, 0};
@@ -178,7 +184,7 @@ float IE[6] = {0, 0, 0, 0, 0, 0};
 float DE[6] = {0, 0, 0, 0, 0, 0};
 float dofa[6] = {0, 0, 0, 0, 0, 0};
 bool JNT_ENABLE[6] = {false, false, false, false, false, false};
-char topic[15], data[10];
+char topic[15], data[10], movetype='J';
 bool move=false, trajenable=false;
 int counter=0, current_buff_id=0, iterations=0, current_iter=0;
 float SPD_OVR=50;
@@ -221,36 +227,231 @@ bool checklimits()
 	return valid;
 }
 
+//Função para conferir os limites angulares em tempo real:
+void realtimecheck(float tol)
+{
+	for(int i=0; i<6; i++)
+	{
+		if((DOFS[i]-limits[i][0])<tol)
+		{
+			if(U[i]<0)
+			{
+				U[i] = 0;
+				char aux[10];
+				aux[0] = '\0';
+				sprintf(aux,"W%d",i);
+				esp_mqtt_client_publish(client, "reply", aux, 0, 0, 0);
+			}
+		}
+		if((limits[i][1]-DOFS[i])<tol)
+		{
+			if(U[i]>0)
+			{
+				U[i] = 0;
+				char aux[10];
+				aux[0] = '\0';
+				sprintf(aux,"W%d",i);
+				esp_mqtt_client_publish(client, "reply", aux, 0, 0, 0);
+			}
+		}
+	}
+}
+
+//Funções para conversão angular:
+float radians(float deg)
+{
+	return ((float)deg*M_PI/180.0);
+}
+float degrees(float rad)
+{
+	return ((float)rad*180/M_PI);
+}
+
+//Função para atualizar os pontos iniciais com a cinemática direta:
+void fwd_kin_p0_up()
+{
+	P0[0] = (-cos(radians(DOFS[0]))*((230*sin(radians(DOFS[1])))+(230*sin(radians(DOFS[1]+DOFS[2])))+(124.5*sin(radians(DOFS[1]+DOFS[2]+DOFS[3])))));
+	P0[1] = (-sin(radians(DOFS[0]))*((230*sin(radians(DOFS[1])))+(230*sin(radians(DOFS[1]+DOFS[2])))+(124.5*sin(radians(DOFS[1]+DOFS[2]+DOFS[3])))));
+	P0[2] = (187+(230*cos(radians(DOFS[1])))+(230*cos(radians(DOFS[1]+DOFS[2])))+(124.5*cos(radians(DOFS[1]+DOFS[2]+DOFS[3]))));
+	float se = (-sin(radians(DOFS[1]+DOFS[2]+DOFS[3])));
+	float ce = cos(radians(DOFS[1]+DOFS[2]+DOFS[3]));
+	float e = degrees(atan2(se,ce));
+	float r = 0;
+	if(fabs(sin(radians(e)))>=1e-6)
+	{
+		float sr = (-sin(radians(DOFS[4]))*sin(radians(DOFS[1]+DOFS[2]+DOFS[3]))/sin(radians(e)));
+		float cr = (-cos(radians(DOFS[4]))*sin(radians(DOFS[1]+DOFS[2]+DOFS[3]))/sin(radians(e)));
+		r = degrees(atan2(sr,cr));
+	}else
+	{
+		r = (DOFS[0]+(DOFS[4]*cos(radians(DOFS[1]+DOFS[2]+DOFS[3]))));
+		if(r>180)
+		{
+			r = (180-r);
+		}
+		if(r< -180)
+		{
+			r = (r+180);
+		}
+	}
+	P0[3] = e;
+	P0[4] = r;
+}
+
+//Função para atualizar as referências com aplicação da cinemática inversa:
+bool inv_kin_ref_up()
+{
+		//Elementos da matriz de rotação:
+		float a;
+        float e = CARTESIAN[3];
+        float r = CARTESIAN[4];
+        if(fabs(sin(radians(e)))>=1e-6)
+        {
+        	a = degrees(atan2(CARTESIAN[1], CARTESIAN[0]));
+        }else
+        {
+        	a = 0;
+        }
+        float r11 = ((cos(radians(a))*cos(radians(e))*cos(radians(r)))-(sin(radians(a))*sin(radians(r))));
+        float r12 = (-(cos(radians(a))*cos(radians(e))*sin(radians(r)))-(sin(radians(a))*cos(radians(r))));
+        float r13 = (cos(radians(a))*sin(radians(e)));
+		float r21 = ((sin(radians(a))*cos(radians(e))*cos(radians(r)))+(cos(radians(a))*sin(radians(r))));
+		float r22 = (-(sin(radians(a))*cos(radians(e))*sin(radians(r)))+(cos(radians(a))*cos(radians(r))));
+		float r23 = (sin(radians(a))*sin(radians(e)));
+		//float r31 = (-sin(radians(e))*cos(radians(r)));
+		//float r32 = (sin(radians(e))*sin(radians(r)));
+		float r33 = cos(radians(e));
+
+        //Desacoplamento cinemático:
+        float xc = (CARTESIAN[0]-(124.5*r13));
+        float yc = (CARTESIAN[1]-(124.5*r23));
+        float zc = (CARTESIAN[2]-(124.5*r33));
+
+        //Vetor para armazenar a solução:
+        float sol[5];
+
+        //Theta 1:
+        sol[0] = degrees(atan2(yc, xc));
+
+        //Theta 3:
+        r = sqrt(pow(xc,2)+pow(yc,2));
+        float h = sqrt(pow(r,2)+pow((zc-187),2));
+        float c3 = (((float)pow(h,2)/(float)(2.0*pow(230,2)))-1);
+        if(fabs(c3)>1)
+        {
+        	esp_mqtt_client_publish(client, "reply", "X", 0, 0, 0);
+        	return false;
+        }
+        float s3 = sqrt(1-pow(c3,2));
+        sol[2] = degrees(atan2(-s3, c3));
+
+        //Theta 2:
+        float alpha = degrees(atan2((zc-187), r));
+        float gamma = degrees(atan2(fabs(-230*sin(radians(sol[2]))), (230*(1+cos(radians(sol[2]))))));
+        sol[1] = (alpha+gamma-90);
+
+        //Theta 4:
+        float s4 = (-(r13*cos(radians(sol[0]))*cos(radians(sol[1]+sol[2])))-(r23*sin(radians(sol[0]))*cos(radians(sol[1]+sol[2])))-(r33*sin(radians(sol[1]+sol[2]))));
+        float c4 = (-(r13*cos(radians(sol[0]))*sin(radians(sol[1]+sol[2])))-(r23*sin(radians(sol[0]))*sin(radians(sol[1]+sol[2])))+(r33*cos(radians(sol[1]+sol[2]))));
+        sol[3] = degrees(atan2(s4, c4));
+
+        //Theta 5:
+        if(fabs(a) >= 1e-3)
+        {
+        	float s5 = (-(r11*sin(radians(sol[0])))+(r21*cos(radians(sol[0]))));
+			float c5 = (-(r12*sin(radians(sol[0])))+(r22*cos(radians(sol[0]))));
+			sol[4] = degrees(atan2(s5, c5));
+        }else
+        {
+        	sol[4] = ((CARTESIAN[4]-sol[0])/cos(radians(sol[1]+sol[2]+sol[3])));
+			if(sol[4]>180)
+			{
+				sol[4] = (180-sol[4]);
+			}
+			if(sol[4]< -180)
+			{
+				sol[4] = (sol[4]+180);
+			}
+        }
+
+        //Atualiza as referências:
+        for(int i=0; i<5; i++)
+        {
+        	REFS[i] = sol[i];
+        }
+
+        return true;
+}
+
+//Função para converter as coordenadas TOOL para BASE:
+void up_cart_base_from_tool(float xt, float yt, float zt)
+{
+	//Elementos da matriz de rotação:
+	float r11 = ((cos(radians(DOFS[0]))*cos(radians(DOFS[1]+DOFS[2]+DOFS[3]))*cos(radians(DOFS[4])))-(sin(radians(DOFS[0]))*sin(radians(DOFS[4]))));
+	float r12 = ((-cos(radians(DOFS[0]))*cos(radians(DOFS[1]+DOFS[2]+DOFS[3]))*sin(radians(DOFS[4])))-(sin(radians(DOFS[0]))*cos(radians(DOFS[4]))));
+	float r13 = (-cos(radians(DOFS[0]))*sin(radians(DOFS[1]+DOFS[2]+DOFS[3])));
+	float r21 = ((sin(radians(DOFS[0]))*cos(radians(DOFS[1]+DOFS[2]+DOFS[3]))*cos(radians(DOFS[4])))-(cos(radians(DOFS[0]))*sin(radians(DOFS[4]))));
+	float r22 = ((-sin(radians(DOFS[0]))*cos(radians(DOFS[1]+DOFS[2]+DOFS[3]))*sin(radians(DOFS[4])))+(cos(radians(DOFS[0]))*cos(radians(DOFS[4]))));
+	float r23 = (-sin(radians(DOFS[0]))*sin(radians(DOFS[1]+DOFS[2]+DOFS[3])));
+	float r31 = (sin(radians(DOFS[1]+DOFS[2]+DOFS[3]))*cos(radians(DOFS[4])));
+	float r32 = (-sin(radians(DOFS[1]+DOFS[2]+DOFS[3]))*sin(radians(DOFS[4])));
+	float r33 = cos(radians(DOFS[1]+DOFS[2]+DOFS[3]));
+
+	//Projeção na base:
+	CARTESIAN[0] =  P0[0]+((xt*r11)+(yt*r12)+(zt*r13));
+	CARTESIAN[1] =  P0[1]+((xt*r21)+(yt*r22)+(zt*r23));
+	CARTESIAN[2] =  P0[2]+((xt*r31)+(yt*r32)+(zt*r33));
+}
+
 //Função para retornar módulo do maior deslocamento angular:
 float maxdelta()
 {
-	float mv = 0;
-	for(int i=0; i<5; i++)
+	if(movetype == 'J')
 	{
-		if(fabs(TARGETS[i]-DOFS[i])>mv)
+		float mv = 0;
+		for(int i=0; i<5; i++)
 		{
-			mv = fabs(TARGETS[i]-DOFS[i]);
+			if(fabs(TARGETS[i]-DOFS[i])>mv)
+			{
+				mv = fabs(TARGETS[i]-DOFS[i]);
+			}
 		}
+		return mv;
 	}
-	return mv;
+	if(movetype == 'L')
+	{
+		return sqrt(pow((CARTESIAN[0]-P0[0]),2)+pow((CARTESIAN[1]-P0[1]),2)+pow((CARTESIAN[2]-P0[2]),2));
+	}
+	return 0;
 }
 
 //Cálculo das constantes para a trajetória:
 void trajCalc()
 {
-	iterations = round((maxdelta()/(float)(SPD_ANG*SPD_OVR*1e-2))/(float)(tt*1e-3));
-	for(int i=0; i<5; i++)
+	if(movetype == 'J')
 	{
-		float q0 = DOFS[i];
-		trajconsts[i][0] = q0;
-		trajconsts[i][1] = (TARGETS[i]-q0)*10/(float)pow(iterations, 3);
-		trajconsts[i][2] = (TARGETS[i]-q0)*-15/(float)pow(iterations, 4);
-		trajconsts[i][3] = (TARGETS[i]-q0)*6/(float)pow(iterations, 5);
+		iterations = round((maxdelta()/(float)(SPD_ANG*SPD_OVR*1e-2))/(float)(tt*1e-3));
+		for(int i=0; i<5; i++)
+		{
+			float q0 = DOFS[i];
+			trajconsts[i][0] = q0;
+			trajconsts[i][1] = (TARGETS[i]-q0)*10/(float)pow(iterations, 3);
+			trajconsts[i][2] = (TARGETS[i]-q0)*-15/(float)pow(iterations, 4);
+			trajconsts[i][3] = (TARGETS[i]-q0)*6/(float)pow(iterations, 5);
+		}
+	}
+	if(movetype == 'L')
+	{
+		iterations = round((maxdelta()/(float)(SPD_LIN*SPD_OVR*1e-2))/(float)(tt*1e-3));
+		for(int i=0; i<5; i++)
+		{
+			linsteps[i] = ((CARTESIAN[i]-P0[i])/(float)iterations);
+		}
 	}
 }
 
 //Calcula o valor da trajetória:
-float trajVal(int joint, int sample)
+float joint_trajVal(int joint, int sample)
 {
 	return (trajconsts[joint][0]+(trajconsts[joint][1]*pow(sample,3))+(trajconsts[joint][2]*pow(sample,4))+(trajconsts[joint][3]*pow(sample,5)));
 }
@@ -258,8 +459,11 @@ float trajVal(int joint, int sample)
 //Função de callback para geração de tragetória:
 static void aftertraj(void* arg)
 {
-	esp_mqtt_client_publish(client, "reply", "J1", 0, 0, 0);
-	jnt_disable_all();
+	char aux[10];
+	aux[0] = '\0';
+	sprintf(aux,"%c%d",movetype,1);
+	esp_mqtt_client_publish(client, "reply", aux, 0, 0, 0);
+	//jnt_disable_all();
 }
 
 //Função de callback para geração de tragetória:
@@ -268,15 +472,31 @@ static void trajgen(void* arg)
 	if(trajenable)
 	{
 		current_iter++;
-		for(int i=0; i<5; i++)
+		if(movetype == 'J')
 		{
-			REFS[i] = trajVal(i, current_iter);
+			for(int i=0; i<5; i++)
+			{
+				REFS[i] = joint_trajVal(i, current_iter);
+			}
+			if(current_iter == iterations)
+			{
+				trajenable = false;
+				current_iter = 0;
+				esp_timer_start_once(aftertraj_timer, 1000e3); //aguarda para desabilitar as o controle das juntas
+			}
 		}
-		if(current_iter == iterations)
+		if(movetype == 'L')
 		{
-			trajenable = false;
-			current_iter = 0;
-			//esp_timer_start_once(aftertraj_timer, 1000e3); //aguarda para desabilitar as o controle das juntas
+			for(int i=0; i<5; i++)
+			{
+				CARTESIAN[i] = (P0[i]+(current_iter*linsteps[i]));
+			}
+			if(!inv_kin_ref_up() || (current_iter==iterations))
+			{
+				trajenable = false;
+				current_iter = 0;
+				esp_timer_start_once(aftertraj_timer, 1000e3); //aguarda para desabilitar as o controle das juntas
+			}
 		}
 	}
 }
@@ -379,6 +599,7 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 				}
 				if(checklimits())
 				{
+					movetype = 'J';
 					trajCalc();
 					current_iter = 0;
 					trajenable = true;
@@ -387,6 +608,50 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base, int32_
 				{
 					esp_mqtt_client_publish(client, "reply", "J0", 0, 0, 0);
 				}
+			}
+			if(data[0] == 'L')	//move linear
+			{
+				movetype = 'L';
+				fwd_kin_p0_up();
+				for(int i=0; i<5; i++)
+				{
+					CARTESIAN[i] = consts[i];
+				}
+				trajCalc();
+				current_iter = 0;
+				trajenable = true;
+				jnt_enable_all();
+			}
+			if(data[0] == 'B')	//move about ... in BASE
+			{
+				movetype = 'L';
+				fwd_kin_p0_up();
+				for(int i=0; i<3; i++)
+				{
+					CARTESIAN[i] = P0[i]+consts[i];
+				}
+				for(int i=3; i<5; i++)
+				{
+					CARTESIAN[i] = P0[i];
+				}
+				trajCalc();
+				current_iter = 0;
+				trajenable = true;
+				jnt_enable_all();
+			}
+			if(data[0] == 'T')	//move about ... in TOOL
+			{
+				movetype = 'L';
+				fwd_kin_p0_up();
+				up_cart_base_from_tool(consts[0], consts[1], consts[2]);
+				for(int i=3; i<5; i++)
+				{
+					CARTESIAN[i] = P0[i];
+				}
+				trajCalc();
+				current_iter = 0;
+				trajenable = true;
+				jnt_enable_all();
 			}
 			if(data[0] == 'D')	//velocidade
 			{
@@ -538,18 +803,15 @@ void PID()
 	{
 		if(JNT_ENABLE[i])
 		{
-			//Eaa[i] = Ea[i];
-			//Ea[i] = E[i];
 			E[i] = REFS[i]-DOFS[i];
-			IE[i] += (E[i]*ts*1e-3);
-			DE[i] = (DOFS[i]-dofa[i])/(float)(ts*1e-3);
+			if((fabs(U[i])<USAT)||(sign(E[i])!=sign(IE[i])))
+			{
+				IE[i] += (E[i]*ts*1e-3);
+			}
+			DE[i] = -(DOFS[i]-dofa[i])/(float)(ts*1e-3);
 			dofa[i] = DOFS[i];
 			u = ((KPID[i][0]*E[i]) + (KPID[i][1]*IE[i]) + (KPID[i][2]*DE[i]));
-			//u = ((KPID[i][1]*ts*1e-3/2.0)+(KPID[i][2]/(float)(ts*1e-3))+KPID[i][0])*E[i];
-			//u += ((KPID[i][1]*ts*1e-3/2.0)-(2*KPID[i][2]/(float)(ts*1e-3))-KPID[i][0])*Ea[i];
-			//u += (KPID[i][2]/(float)(ts*1e-3))*Eaa[i];
 			u += sign(u)*ZM[i][sign(u)+1];
-
 			U[i] = sat(u, -USAT, USAT);
 		}
 	}
@@ -559,8 +821,11 @@ void PID()
 		float refth4_ = (REFS[3]+(REFS[4]*L/2.0));
 		float th4_ = (DOFS[3]+(DOFS[4]*L/2.0));
 		E[3] = refth4_-th4_;
-		IE[3] += (E[3]*ts*1e-3);
-		DE[3] = (DOFS[3]-dofa[3])/(float)(ts*1e-3);
+		if((fabs(U[3])<USAT)||(sign(E[3])!=sign(IE[3])))
+		{
+			IE[3] += (E[3]*ts*1e-3);
+		}
+		DE[3] = -(DOFS[3]-dofa[3])/(float)(ts*1e-3);
 		dofa[3] = DOFS[3];
 		u = ((KPID[3][0]*E[3]) + (KPID[3][1]*IE[3]) + (KPID[3][2]*DE[3]));
 		u += sign(u)*ZM[3][sign(u)+1];
@@ -571,8 +836,11 @@ void PID()
 		float refth5_ = (REFS[3]-(REFS[4]*L/2.0));
 		float th5_ = (DOFS[3]-(DOFS[4]*L/2.0));
 		E[4] = refth5_-th5_;
-		IE[4] += (E[4]*ts*1e-3);
-		DE[4] = (DOFS[4]-dofa[4])/(float)(ts*1e-3);
+		if((fabs(U[4])<USAT)||(sign(E[4])!=sign(IE[4])))
+		{
+			IE[4] += (E[4]*ts*1e-3);
+		}
+		DE[4] = -(DOFS[4]-dofa[4])/(float)(ts*1e-3);
 		dofa[4] = DOFS[4];
 		u = ((KPID[4][0]*E[4]) + (KPID[4][1]*IE[4]) + (KPID[4][2]*DE[4]));
 		u += sign(u)*ZM[4][sign(u)+1];
@@ -582,8 +850,11 @@ void PID()
 	if(JNT_ENABLE[5])
 	{
 		E[5] = REFS[5]-DOFS[5];
-		IE[5] += (E[5]*ts*1e-3);
-		DE[5] = (DOFS[5]-dofa[5])/(float)(ts*1e-3);
+		if((fabs(U[5])<USAT)||(sign(E[5])!=sign(IE[5])))
+		{
+			IE[5] += (E[5]*ts*1e-3);
+		}
+		DE[5] = -(DOFS[5]-dofa[5])/(float)(ts*1e-3);
 		dofa[5] = DOFS[5];
 		u = ((KPID[5][0]*E[5]) + (KPID[5][1]*IE[5]) + (KPID[5][2]*DE[5]));
 		u += sign(u)*ZM[5][sign(u)+1];
@@ -650,6 +921,7 @@ static void control(void* arg)
 	if(move)
 	{
 		PID();
+		realtimecheck(5);
 		updatePWM();
 	}
 
@@ -805,7 +1077,7 @@ void app_main()
 	timers();
 
 	//Aguarda filtros acomodarem e atualiza a configuração articular inicial do robô:
-	sleep(1);
+	sleep(2);
 	for(int i=0; i<6; i++)
 	{
 		REFS[i] = DOFS[i];
